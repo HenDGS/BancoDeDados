4ad7cef28576b5f5fc255b86aa4f73ee267f3afb
Henrique da Gama Saczkowski <henrique1443@hotmail.com>
2022-10-27 01:18:15 -0300

    Update

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import numpy as np
 from mysql.connector import Error
 import PySimpleGUI as sg
 
-
 # Function to read from mysql database
 '''def read_from_mysql():
     global cursor
@@ -28,23 +27,68 @@ import PySimpleGUI as sg
     except Error as e:
         print("Error while connecting to MySQL", e)'''
 
-# Function to search csv data by column
-def search_by_column(data, column, value):
-    return data[data[:, column] == value]
-
-
 # Function to read from csv file
 def read_from_csv():
-    data = np.genfromtxt('data.csv', delimiter=',')
-    print(data)
+    data = np.genfromtxt('employees1000.csv', delimiter=',', dtype=None, encoding=None)
+    # data = np.genfromtxt('data.csv', delimiter=',')
     return data
+
+# Function to recive an input with projection, where and order by; then search in the data
+def search(data):
+    # Layout
+    layout = [
+        [sg.Text('Enter the projection: '), sg.Input(key='projection')],
+        [sg.Text('Enter the where: '), sg.Input(key='where')],
+        [sg.Text('Enter the order by: '), sg.Input(key='order')],
+        [sg.Button('Search')]
+    ]
+    # Create window
+    window = sg.Window('Search', layout)
+    # Read values
+    event, values = window.read()
+    # Close window
+    window.close()
+    # Get values
+    projection = values['projection']
+    where = values['where']
+    order = values['order']
+    # Search in the data
+    if where == '':
+        if order == '':
+            if projection == '':
+                print(data)
+            else:
+                print(data[projection])
+        else:
+            if projection == '':
+                print(data[np.argsort(data[order])])
+            else:
+                print(data[projection][np.argsort(data[order])])
+    else:
+        if order == '':
+            if projection == '':
+                print(data[data[where]])
+            else:
+                print(data[projection][data[where]])
+        else:
+            if projection == '':
+                print(data[data[where]][np.argsort(data[order])])
+            else:
+                print(data[projection][data[where]][np.argsort(data[order])])
+
+
+# Function to search by
 
 
 # Main function
 def main():
     data = read_from_csv()
+    # Print columns in data
+    print(data.dtype.names)
+    # Search in data
+    search(data)
 
-    # Query data
+    print(data[0])
 
 
 # Run main function
